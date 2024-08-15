ee3f542f550db17b2dc52dec1688e2e47425c298
Phattarawadee-K <phattarawadee.ksw@gmail.com>
2024-08-15 21:49:14 +0700

    [edit] app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          html ok na
+          html ok na edit
         </p>
         <a
           className="App-link"

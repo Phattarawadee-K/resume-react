9f720dcbbdba6265a490bf5e80452baf9ba8bf2d
Phattarawadee-K <phattarawadee.ksw@gmail.com>
2024-08-15 21:52:02 +0700

    อีโง่

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          html ok na edit
+          อีโง่
         </p>
         <a
           className="App-link"
